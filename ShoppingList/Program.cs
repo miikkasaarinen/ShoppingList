b2771cdf924a79a676c8abb73f0f2b55077dc5f8
Miikka Saarinen <miikka.saarinen@awacademy.training>
2022-06-08 10:13:07 +0300

    Aloitus

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
