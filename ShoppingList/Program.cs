449461328a74a6ad8d61433cdec0d9a4dabba2ca
Miikka Saarinen <miikka.saarinen@awacademy.training>
2022-06-08 11:02:57 +0300

    Versio 1.0

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace ShoppingList
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string file = @"C:\Users\Miikka\source\repos\ShoppingList\files\shoppingList.txt";
+
+            while (true)
+            {
+                var tuotteet = new List<string>(); // Luodaan lista tuotteille
+
+
+                Console.Write("Lisää tuote ostoslistaan: ");
+                string tuote = Console.ReadLine();
+
+                tuotteet.Add(tuote); // Lisää tuotteen listaan
+
+                // Kirjoittaa tuotteen tiedostoon shoppingList.txt
+                File.AppendAllLines(@"C:\Users\Miikka\source\repos\ShoppingList\files\shoppingList.txt", tuotteet);
+
+
+                // Ohjelman jatkaminen tai lopettaminen
+                Console.Write("Paina 'enter' jatkaakseni tai kirjoita 'exit' lopettaaksesi: ");
+                string exit = Console.ReadLine();
+                exit.ToLower();
+
+
+                // Tyhjentää konsolin
+                Console.Clear();
+
+                if (exit == "exit")
+                    break;
+            }
+
         }
     }
 }
